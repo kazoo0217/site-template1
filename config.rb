3e16126bf787bc060afc1ca6b3313fe8b3ccae1c
Kazuhito Tsujita <kazuhito.tsujita@gmail.com>
2013-02-11 12:34:27 +0900

    update config rb

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "img"
+images_dir = "/"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :nested
+output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
