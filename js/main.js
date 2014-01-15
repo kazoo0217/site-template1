19979357c66f182b3880694e6872b337cbb64438
Kazuhito Tsujita <kazuhito.tsujita@gmail.com>
2014-01-15 14:06:42 +0900

    add main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  //Smooth Scroll
+  $('#page-top, .pagetop').click(function() {
+    $.smoothScroll({
+      speed: 600
+    });
+    return false;
+  });
+
+  //other script
+  
+});
